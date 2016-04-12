dc6a2cada919f67c49b5924b7e9ab20428a1641e
shanewoms13 <15Womels7081@pit.edu>
2016-04-12 18:20:10 +0000

    first commit

---
+++ cv/main.js
@@ -0,0 +1,16 @@
+//-------------------------------
+
+
+
+
+//---------------------------------
+window.onload = resizeRootEm;
+window.onresize = resizeRootEm;
+
+
+
+
+//----------------------------------
+function resizeRootEm(){
+    document.documentElement.style.fontSize = 10 + window.innerWidth/100 + "px";
+}
