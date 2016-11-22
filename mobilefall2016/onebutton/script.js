e97d7cc78635eb815fc18517133a37f2a6c92596
shanewoms13 <15Womels7081@pit.edu>
2016-11-22 16:14:17 +0000

    onebutton

--- mobilefall2016/onebutton/script.js
+++ mobilefall2016/onebutton/script.js
@@ -11,9 +11,9 @@ function eventCallback (e){
 <script>
 $( "button" )
   .mouseup(function() {
-    $( this ).append( "<span style='color:#f00;'>Mouse up.</span>" );
+    $( this ).append( "<span style='color:#f00;'>Mouse up.</span>" )
   })
   .mousedown(function() {
-    $( this ).append( "<span style='color:#00f;'>Mouse down.</span>" );
+    $( this ).append( "<span style='color:#00f;'>Mouse down.</span>" )
   });
 </script>
