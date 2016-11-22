0aa21a6db6cba9b9705e52007b3f617025fe9634
shanewoms13 <15Womels7081@pit.edu>
2016-11-22 16:12:37 +0000

    onebutton

--- mobilefall2016/onebutton/script.js
+++ mobilefall2016/onebutton/script.js
@@ -8,7 +8,12 @@ function eventCallback (e){
 
 //attach event handlers to the button
 //for each of the four events mention the button
-.mousedown()
-.mouseup()
-.mouseover()
-.mouseout()
+<script>
+$( "p" )
+  .mouseup(function() {
+    $( this ).append( "<span style='color:#f00;'>Mouse up.</span>" );
+  })
+  .mousedown(function() {
+    $( this ).append( "<span style='color:#00f;'>Mouse down.</span>" );
+  });
+</script>
