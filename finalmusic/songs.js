c7d2c05cd996a13d59966495824bf25adc381c7e
shanewoms13 <15Womels7081@pit.edu>
2016-12-16 15:18:28 +0000

    memo

---
+++ finalmusic/songs.js
@@ -0,0 +1,19 @@
+/*
+	Each song is an .mp3
+	Each picture is a .png
+*/
+var songs = {
+	1:{
+		song:"Queen - We Will Rock You",
+		picture: "Queen"
+	},
+	'2':{
+		song:"Star Wars - Star Wars",
+		picture: "Starwars"
+	},
+	3:{
+		song:"The Weeknd - Starboy",
+		picture: "Weeknd"
+	}
+};
+
